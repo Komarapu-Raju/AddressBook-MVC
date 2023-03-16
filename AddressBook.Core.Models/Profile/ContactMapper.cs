4ef06bac88c1c927c630a83139d41a7cab80f5d0
Komarapu-Raju <raju.k@keka.com>
2023-03-16 16:51:33 +0530

    dfcds

---
+++ AddressBook.Core.Models/Profile/ContactMapper.cs
@@ -0,0 +1,15 @@
+﻿using AddressBook.Core.Models.ViewModels;
+using AddressBook.Data.Models.Contact;
+using AutoMapper;
+
+namespace AddressBook.Helpers
+{
+    public class ContactMapper : Profile
+    {
+        public ContactMapper()
+        {
+            CreateMap<Contact, ContactDetailsViewModel>().ReverseMap();
+            CreateMap<Contact, ContactListViewModel>().ReverseMap();
+        }
+    }
+}
